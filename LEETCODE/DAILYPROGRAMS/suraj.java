7872eb64aa1ed72487429458a0d85037bd222546
surajbombale05 <babanbombale59@gmail.com>
2023-12-21 17:48:12 +0530

    main

---
+++ LEETCODE/DAILYPROGRAMS/suraj.java
@@ -0,0 +1,8 @@
+public class suraj {
+    public static void main(String args[]){
+        int a = 13;
+        int b = 45;
+        int c = a+b;
+        System.out.println("addition is :-"+c);
+    }
+}
